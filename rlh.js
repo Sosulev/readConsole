013deb37b021743bd611bc34e04e1c8b158fe1e2
Sosulev <sosulev@gmail.com>
2025-01-04 15:03:09 +0300

    first commit

---
+++ rlh.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const os = require('os').homedir()
+const readline = require('readline')
+const fs = require('fs')
+const rl = readline.createInterface({input: process.stdin, output: process.stdout, prompt: '=> ', historySize: 100})
+const historyFile = path.join(os, ".test_command_history")
+try { rl.history = JSON.parse(fs.readFileSync(historyFile, "utf-8")) } catch (error) {}
+
+rl.on('SIGINT', () => rl.close())
+rl.on('history', (history) => fs.writeFileSync(historyFile, JSON.stringify(history)))
+rl.on('line', (input) => {
+  input = input.replace(/ +/g, ' ').trim()
+  const [command, ...arr] = input.split(' ')
+  if(rl.listenerCount(command)) rl.emit(command, arr)
+  else console.log(`The command '${command}' was not found`)
+  rl.prompt()
+})
+rl.prompt()
+
+rl.on('test', arr=>{console.log(arr)})
+
+module.exports = rl
