b40755362c1ac3b314a4833bd2f0caec6bdf0ffb
Sosulev <sosulev@gmail.com>
2025-01-05 12:56:26 +0300

    new

--- readConsole.js
+++ readConsole.js
@@ -1,7 +1,8 @@
-const path = require('path')
-const os = require('os').homedir()
-const readline = require('readline')
-const fs = require('fs')
+import path from 'path'
+import readline from 'readline'
+import fs from 'fs'
+import osData from 'os'
+const os=osData.homedir()
 const rl = readline.createInterface({input: process.stdin, output: process.stdout, prompt: '=> ', historySize: 100})
 const historyFile = path.join(os, ".test_command_history")
 try { rl.history = JSON.parse(fs.readFileSync(historyFile, "utf-8")) } catch (error) {}
@@ -9,14 +10,15 @@ try { rl.history = JSON.parse(fs.readFileSync(historyFile, "utf-8")) } catch (er
 rl.on('SIGINT', () => rl.close())
 rl.on('history', (history) => fs.writeFileSync(historyFile, JSON.stringify(history)))
 rl.on('line', (input) => {
-  input = input.replace(/ +/g, ' ').trim()
-  const [command, ...arr] = input.split(' ')
-  if(rl.listenerCount(command)) rl.emit(command, arr)
-  else console.log(`The command '${command}' was not found`)
+  if(input != '') {
+    input = input.replace(/ +/g, ' ').trim()
+    const [command, ...arr] = input.split(' ')
+    if(rl.listenerCount(command)) rl.emit(command, arr)
+    else console.log(`The command '${command}' was not found`)
+  }
   rl.prompt()
 })
 rl.prompt()
-
 rl.on('test', console.log)
 
-module.exports = rl
+export default rl
